6972192d00dfa168b99c6c61f5e375085d4b68d7
El-Nouty <69649680+delnouty@users.noreply.github.com>
2026-04-08 22:52:47 +0200

    Update config.py

--- automobile-dashboard/dashboard/config.py
+++ automobile-dashboard/dashboard/config.py
@@ -1,1 +1,28 @@
+# config.py
+
+COLOR_MAP = {
+    'Supperminicar': '#006BA4',
+    'Mediumfamilycar': '#FF800E',
+    'Smallfamiliycar': '#ABABAB',
+    'Sports': '#595959',
+    'Executivecar': '#5F9ED1'
+}
+
+MONTH_ORDER = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+VEHICLE_TYPE_ORDER = [
+    'Mediumfamilycar',
+    'Smallfamiliycar',
+    'Supperminicar',
+    'Sports',
+    'Executivecar'
+]
+
+YEAR_LIST = [i for i in range(1980, 2024)]
+
+DROPDOWN_OPTIONS = [
+    {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
+    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
+]
 
